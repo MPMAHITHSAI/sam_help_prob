9d1102dcd9b5216dfbe9854649abd0188af7dd68
MPMAHITHSAI <mpmahithsai@gmail.com>
2023-07-20 11:11:35 +0530

    sam helped for no of question

---
+++ samhelp.java
@@ -0,0 +1,13 @@
+package Helpfromsam;
+import java.util.*;
+
+public class samhelp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        if(t%2==0){
+            System.out.println("1");
+        } else{
+            System.out.println("2");
+        }
+    }}
